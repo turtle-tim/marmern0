b0ba3b9dc366bd2c057cccc75b4fdf469bcb6fbb
turtle-tim <149429545+turtle-tim@users.noreply.github.com>
2024-03-11 14:18:24 -0500

    Update root.js
    My dumbass forgot the "module.exports=router," resulting in "TypeError: Router.use() requires a middleware function but got a Object"

--- route/root.js
+++ route/root.js
@@ -5,3 +5,4 @@ const router=require("express").Router()
 const path=require("path")
 
 router.get("^/$|/index(.html)?",(_,res)=>res.sendFile(path.join(__dirname,"..","views","index.html")))
+module.exports=router
